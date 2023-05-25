143103aa39fa62519f3395f51ae59ddfb2612044
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2023-05-25 15:16:32 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,75 @@
+# I couldnt get this fully working but this is where I am at... (try it with a mediawiki docker, and a bookstack docker!)
+
+import requests
+from bs4 import BeautifulSoup
+import bookstack
+
+# Get the MediaWiki page
+url = "https://example.com/wiki/Main_Page"
+response = requests.get(url)
+
+# Parse the page with BeautifulSoup
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find all the article titles
+article_titles = soup.find_all("a", class_="mw-jump-link")
+
+# Create a list of article titles
+article_titles_list = []
+for article_title in article_titles:
+    article_titles_list.append(article_title.text)
+
+# Import the articles into Bookstack
+for article_title in article_titles_list:
+    print("Importing article:", article_title)
+
+    # Get the article content
+    url = "https://example.com/wiki/" + article_title
+    response = requests.get(url)
+
+    # Parse the page with BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Get the article body
+    article_body = soup.find("div", class_="mw-content-ltr")
+
+    # Create a Bookstack article
+    article = {
+        "title": article_title,
+        "body": article_body.text,
+    }
+
+    # Import the article into Bookstack
+    bookstack.import_article(article, api_key="YOUR_API_KEY")
+
+# Create a Bookstack object
+bookstack_client = bookstack.Client(url="https://my-bookstack-url.com")
+
+# Connect to Bookstack
+bookstack_client.connect()
+
+# Import the articles into Bookstack
+for article in article_titles_list:
+    print("Importing article:", article)
+
+    # Get the article content
+    url = "https://example.com/wiki/" + article
+    response = requests.get(url)
+
+    # Parse the page with BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Get the article body
+    article_body = soup.find("div", class_="mw-content-ltr")
+
+    # Create a Bookstack article
+    article = {
+        "title": article_title,
+        "body": article_body.text,
+    }
+
+    # Import the article into Bookstack
+    bookstack_client.import_article(article)
+
+# Disconnect from Bookstack
+bookstack_client.disconnect()
